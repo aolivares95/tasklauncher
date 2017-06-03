a648f8063706b52af3ceeed5ea3e5d5286c5e950
aolivares95 <antonio.olivares1995@gmail.com>
2017-06-03 14:26:52 -0400

    added apps list activity and home activity

---
+++ app/src/main/java/com/example/aj/tasklauncher/AppsListActivity.java
@@ -0,0 +1,16 @@
+package com.example.aj.tasklauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AppsListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+    }
+
+}
